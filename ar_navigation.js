2c971ddbfe1b4560cd8ba6a862a55f5313006530
tisya207 <65344664+tisya207@users.noreply.github.com>
2023-10-18 20:04:32 +0530

    Add files via upload

--- ar_navigation.js
+++ ar_navigation.js
@@ -1,6 +1,7 @@
 let coordinates= {}
 $(document).ready(function (){
     get_coordinates()
+    render_elements()
 })
 
 function get_coordinates(){
@@ -22,4 +23,72 @@ function get_coordinates(){
         alert("Coordinates not selected");
         window.history.back();
     }
+}
+
+function render_elements(){
+    $.ajax({
+        url: `https://api.mapbox.com/directions/v5/mapbox/driving/${coordinates.source_lon}%2C${coordinates.source_lat}%3B${coordinates.destination_lon}%2C${coordinates.destination_lat}?alternatives=true&geometries=polyline&steps=true&access_token=pk.eyJ1IjoiY2xlb3BhdHJhMSIsImEiOiJjbG5idDlzM2swM3pnMmp1Zmx2eWljbDFyIn0.8jmp8LtiLdi6ekn-7sqBKw`,
+        type:"get",
+        success: function(response){ 
+               
+            let images = {
+                "turn_right": "ar_right.png",
+                "turn_left": "ar_left.png",
+                "straight": "ar_straight.png",
+            }
+
+            let steps = response.routes[0].legs[0].steps
+            for (let i= 0 ; i<steps.length; i++ ) {
+                let image; 
+                let distance= steps[i].distance
+                let instruction = steps[i].maneuver.instruction
+                
+                if (instruction.includes("Turn right")){
+                    image = "turn_right"
+                }
+                else if(instruction.includes("Turn left")){
+                    image= "turn_left"
+                }
+            
+                if(i>0){
+                    $("#scene_container").append(
+                        `
+                            <a-entity gps-entity-place="latitude: ${steps[i].maneuver.location[1]};longitude: ${steps[i].maneuver.longitude[0]};" >
+                                <-a-image 
+                                name = "${instruction}"
+                                src = "./assets/${images[image]}"
+                                look-at = "#step_${i-1}"
+                                id = "step_${i}"
+                                position = "0 0 0"
+                                scale = "5 5 5"
+                                ></a-image>
+                                <a-entity>
+                                    <a-text height ="50" value = "${instruction} (${distance}m)"></a-text>
+                                </a-entity>
+                            </a-entity>
+                        `
+                    )
+                }else {
+                    $("#scene_container").append(
+                        `
+                            <a-entity gps-entity-place="latitude: ${steps[i].maneuver.location[1]};longitude: ${steps[i].maneuver.longitude[0]};" >
+                                <-a-image 
+                                name = "${instruction}"
+                                src = "./assets/ar_start.png"
+                                look-at = "#step_${i+1}"
+                                id = "step_${i}"
+                                position = "0 0 0"
+                                scale = "5 5 5"
+                                ></a-image>
+                                <a-entity>
+                                    <a-text height ="50" value = "${instruction} (${distance}m)"></a-text>
+                                </a-entity>
+                            </a-entity>
+                        `
+                    )
+                }
+            }
+
+        }
+    })
 }
